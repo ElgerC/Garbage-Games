03b80c4953e3377e5d4edb4b5fdc6e7070bfff9f
ElgerC <elger.creutzberg@gmail.com>
2024-11-18 14:29:41 +0100

    working on bugfix

---
+++ Assets/Developers/Elger/Scripts/DashBarrierScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashBarrierScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("test");
+        if (other.transform.tag == "Player")
+        {
+            Debug.Log("grah");
+            Vector3 movement = new Vector3(-transform.forward.x, 0.1f, -transform.forward.z);
+            other.gameObject.GetComponent<Rigidbody>().AddForce(movement * 5, ForceMode.Impulse);
+        }
+    }
+}
--- Assets/Developers/Elger/Scripts/PlayerScript.cs
+++ Assets/Developers/Elger/Scripts/PlayerScript.cs
@@ -2,36 +2,102 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+public enum states
+{
+    minigame1,
+    minigame2,
+    minigame3,
+    minigame4,
+    minigame5,
+}
 public class PlayerScript : MonoBehaviour
 {
-    [SerializeField] private float playerSpeed = 2.0f;
-    [SerializeField] private float gravityValue = -9.81f;
-    private Vector3 playerVelocity;
-    [SerializeField] private float playerHealth;
-    [SerializeField] private float playerKnockDownHealth;
+    [SerializeField] private float playerSpeed;
 
-    private CharacterController characterController;
+    private Rigidbody rb;
 
     private Vector2 movementInput = Vector2.zero;
-    void Start()
+
+    [SerializeField] states state;
+
+    [SerializeField] private bool canAct = true;
+
+    [SerializeField] private float dashSpeed;
+
+    [SerializeField] private GameObject barrier;
+
+    private void Awake()
     {
-        characterController = gameObject.GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
+            rb.mass += 1;
             movementInput = context.ReadValue<Vector2>();
         }
-
     }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (context.performed && canAct)
+        {
+            switch (state)
+            {
+                case states.minigame1:
+                    rb.mass = 1;
+
+                    Vector3 movement = new Vector3(transform.forward.x, 0.1f, transform.forward.z);
+                    rb.AddForce(movement * dashSpeed, ForceMode.Impulse);
+
+                    barrier.SetActive(true);
+
+                    StartCoroutine(ActionCD(4));
+                    break;
+                case states.minigame2:
+                    Debug.Log(state);
+                    break;
+                case states.minigame3:
+                    Debug.Log(state);
+                    break;
+                case states.minigame4:
+                    Debug.Log(state);
+                    break;
+                case states.minigame5:
+                    Debug.Log(state);
+                    break;
+            }
+        }
+    }
+
+    private IEnumerator ActionCD(float cooldown)
+    {
+        canAct = false;
+        yield return new WaitForSeconds(cooldown);
+        canAct = true;
+    }
+
 
     void Update()
     {
-        Vector3 move = new Vector3(movementInput.x, 0, movementInput.y);
-        characterController.Move(move * Time.deltaTime * playerSpeed);
-        /*        characterController.Move(playerVelocity * Time.deltaTime);*/
+        Vector3 move = new Vector3(movementInput.x, 0, movementInput.y).normalized * playerSpeed;
+        rb.MovePosition(transform.position + move);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        if (rb.velocity.z < 1 && rb.velocity.z > -1)
+        {
+            barrier.SetActive(false);
+        }
+        else
+        {
+            barrier.SetActive(true);
+        }
+
     }
 }
