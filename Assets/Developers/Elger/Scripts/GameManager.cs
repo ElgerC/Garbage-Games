8ebc11ed4a513ecb8c6baf8846825976d3985730
ElgerC <elger.creutzberg@gmail.com>
2024-11-19 13:31:51 +0100

    working on GameManager

--- Assets/Developers/Elger/Scripts/GameManager.cs
+++ Assets/Developers/Elger/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,16 +10,22 @@ public class Gamemanager : MonoBehaviour
     [SerializeField] private Vector3 startPos;
 
     public List<GameObject> players = new List<GameObject>();
-    public List<Scene> minigames = new List<Scene>();
+    public List<string> minigames = new List<string>();
+    public List<string> playedMinigames = new List<string>();
+
+    [SerializeField] private Vector3[][] spawnPositions;
     private void Awake()
     {
         if (instance == null)
             instance = this;
         else
             Destroy(this);
+
+        DontDestroyOnLoad(gameObject);
     }
     private void Start()
     {
+        SceneManager.LoadScene(ChooseScene());
         transform.position = startPos;
     }
     public void AddPlayer(GameObject obj)
@@ -28,5 +35,21 @@ public class Gamemanager : MonoBehaviour
         obj.transform.position = transform.position;
 
         transform.position = new Vector3(transform.position.x + 5f, startPos.y, startPos.z);
+    }
+    private string ChooseScene()
+    {
+        string scene = null;
+        while(scene == null)
+        {
+            string posibleScene = minigames[Random.Range(0, minigames.Count)];
+            if(!playedMinigames.Contains(posibleScene))
+                scene = posibleScene;
+        }
+        return scene;   
+    }
+    public void MinigameFinished()
+    {
+        playedMinigames.Add(SceneManager.GetActiveScene().name);
+        ChooseScene();
     }
 }
