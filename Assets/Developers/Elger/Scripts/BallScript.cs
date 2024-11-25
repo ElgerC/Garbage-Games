6d28ec56c2e8f7da559e8353d2d1f287fa852ca7
ElgerC <elger.creutzberg@gmail.com>
2024-11-25 13:39:30 +0100

    golfball base af

--- Assets/Developers/Elger/Scripts/BallScript.cs
+++ Assets/Developers/Elger/Scripts/BallScript.cs
@@ -5,28 +5,57 @@ using UnityEngine;
 public class BallScript : MonoBehaviour
 {
     private Rigidbody RB_rb;
+    private Animator Anim_animator;
+
     [SerializeField] private Vector3 V3_direction;
     [SerializeField] private float F_speed;
-    Vector3 velo;
+
     private Vector3 MinSpeed = new Vector3(0.5f, 0.5f, 0.5f);
+
+    [SerializeField] private Quaternion Q_SavedRotation;
+    private bool B_SavedRot = false;
+
+    private GameObject G_Pivot;
     private void Awake()
     {
         RB_rb = GetComponent<Rigidbody>();
-        V3_direction = transform.forward * F_speed;
+        Anim_animator = GetComponent<Animator>();
+        G_Pivot = transform.GetChild(0).gameObject;
     }
-    private void Start()
+    public void LockRotation()
     {
+        Q_SavedRotation = G_Pivot.transform.rotation;
+        B_SavedRot = true;
+
+        Anim_animator.SetTrigger("LockRotate");
+    }
+    public void Launch()
+    {
+        V3_direction = transform.forward * F_speed * G_Pivot.transform.GetChild(0).localScale.z;
+        Debug.Log(RB_rb.velocity);
+
+        Anim_animator.enabled = false;
+        G_Pivot.SetActive(false);
+
+        
         RB_rb.AddForce(V3_direction);
-    }
-    private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log(velo);
-        RB_rb.AddForce(Vector3.Reflect(V3_direction, collision.contacts[0].normal));
-        V3_direction = Vector3.Reflect(velo, collision.contacts[0].normal);
     }
     private void Update()
     {
-        if (RB_rb.velocity.magnitude > MinSpeed.magnitude)
-            velo = RB_rb.velocity;
+        if(RB_rb.velocity.magnitude < MinSpeed.magnitude)
+        {
+            RB_rb.velocity = Vector3.zero;
+        }
+        if(Input.GetMouseButtonDown(0))
+        {
+            LockRotation();
+        } else if (Input.GetMouseButtonUp(0))
+        {
+            Launch();
+        }
+        if(B_SavedRot)
+        {
+            transform.rotation = Q_SavedRotation;
+        }
     }
 }
