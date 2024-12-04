9566876be339c2ead82e5b2c36ba196c651864c5
ElgerC <elger.creutzberg@gmail.com>
2024-12-04 08:36:28 +0100

    almost doen with mashers

---
+++ Assets/Artists/Stijn/textures/Developers/Elger/Scripts/Masher/ThrowScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowScript : MonoBehaviour
+{
+    public void Deliverd()
+    {
+        Destroy(gameObject);
+    }
+}
